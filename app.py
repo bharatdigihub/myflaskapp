ee0ae06d314945b8fada80ff691a030e025ecf47
bharatdigihub <bk.digitalservice@gmail.com>
2025-10-28 18:59:00 +0530

    update all files for postgrsql

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
 from flask import Flask
-import mysql.connector
+import psycopg2
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    db_connection = mysql.connector.connect(
-        host=os.environ.get("DB_HOST"),
-        user=os.environ.get("DB_USER"),
-        password=os.environ.get("DB_PASSWORD"),
-        database=os.environ.get("DB_NAME")
-    )
-    cursor = db_connection.cursor()
-    cursor.execute("SELECT DATABASE();")
-    db_name = cursor.fetchone()
-    cursor.close()
-    db_connection.close()
-    return f"Connected to MySQL database: {db_name[0]}"
+def index():
+    try:
+        conn = psycopg2.connect(
+            host=os.environ.get("DB_HOST"),
+            database=os.environ.get("DB_NAME"),
+            user=os.environ.get("DB_USER"),
+            password=os.environ.get("DB_PASSWORD"),
+            port=os.environ.get("DB_PORT")
+        )
+        cur = conn.cursor()
+        cur.execute("SELECT current_database();")
+        db_name = cur.fetchone()
+        cur.close()
+        conn.close()
+        return f"✅ Connected to PostgreSQL database: {db_name[0]}"
+    except Exception as e:
+        return f"❌ Database connection failed: {e}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
